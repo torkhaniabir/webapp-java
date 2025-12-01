5da61e6e529410f8dce91687254b60b92d258836
torkhaniabir <torkhaniabir2@gmail.com>
2025-12-01 09:53:16 +0100

    Add unit tests + Jenkins auto run stage + port config

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM openjdk:17-jdk-slim
+FROM eclipse-temurin:17-jdk-jammy
+VOLUME /tmp
+ARG JAR_FILE=target/demo-1.0.0.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 9090
+ENTRYPOINT ["java","-jar","/app.jar"]
 
-WORKDIR /app
-
-COPY target/demo-1.0.0.jar app.jar
-
-EXPOSE 8080
-
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestRestController
+@RestController
 public class DemoApplication {
 
     @GetMapping("/")
